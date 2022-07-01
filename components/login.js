b97a20d47f5375399d4d462bb0c1fc362c071007
FlaviaBrunetta <flaviabdaboit@gmail.com>
2022-07-01 17:56:37 -0400

    Implementação de tema e novas modificações na navbar/login

--- components/login.js
+++ components/login.js
@@ -1,76 +1,80 @@
 export default function Login() {
   return (
     <>
-      <label for="my-modal" className="btn btn-ghost">
-        login
-      </label>
+      <div class="flex-none">
+        <label for="my-modal" className="btn btn-ghost btn-circle avatar">
+          <img
+            className="rounded-full"
+            src="https://placeimg.com/80/80/people"
+          />
+        </label>
 
-      <input type="checkbox" id="my-modal" className="modal-toggle" />
-      <div className="modal fixed inset-0 bg-opacity-0 transition-opacity">
-        <div className="modal-box ">
-          <label
-            for="my-modal"
-            className="btn btn-sm btn-circle absolute right-2 top-2"
-          >
-            ✕
-          </label>
+        <input type="checkbox" id="my-modal" className="modal-toggle" />
+        <div className="modal fixed inset-0 bg-opacity-0 transition-opacity">
+          <div className="modal-box ">
+            <label
+              for="my-modal"
+              className="btn btn-sm btn-circle absolute right-2 top-2"
+            >
+              ✕
+            </label>
 
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Login
-            </h2>
-          </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" value="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label for="email-address" className="sr-only">
-                  Email
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autocomplete="email"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none  focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email"
-                />
-              </div>
-              <div>
-                <label for="password" className="sr-only">
-                  Senha
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autocomplete="current-password"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Senha"
-                />
-              </div>
+            <div>
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Login
+              </h2>
             </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                />
-                <label
-                  for="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  {' '}
-                  Salvar senha{' '}
-                </label>
+            <form className="mt-8 space-y-6" action="#" method="POST">
+              <input type="hidden" name="remember" value="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label for="email-address" className="sr-only">
+                    Email
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none  focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Email"
+                  />
+                </div>
+                <div>
+                  <label for="password" className="sr-only">
+                    Senha
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autocomplete="current-password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Senha"
+                  />
+                </div>
               </div>
 
-              {/* <div className="text-sm">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                  <label
+                    for="remember-me"
+                    className="ml-2 block text-sm text-gray-900"
+                  >
+                    {' '}
+                    Salvar senha{' '}
+                  </label>
+                </div>
+
+                {/* <div className="text-sm">
                 <a
                   href="#"
                   className="font-medium text-indigo-600 hover:text-indigo-500"
@@ -79,17 +83,18 @@ export default function Login() {
                   Forgot your password?{' '}
                 </a>
               </div> */}
-            </div>
+              </div>
 
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,30 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#6b7280',
+
+          secondary: '#60a5fa',
+
+          accent: '#37CDBE',
+
+          neutral: '#3D4451',
+
+          'base-100': '#FFFFFF',
+
+          info: '#3ABFF8',
+
+          success: '#36D399',
+
+          warning: '#FBBD23',
+
+          error: '#F87272',
+        },
+      },
+    ],
+  },
   plugins: [require('daisyui')],
 };
